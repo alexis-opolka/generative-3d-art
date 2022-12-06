17e52a6ce2006981e1c01625875cb9d618e20d70
alexis-opolka <Alexis.opoka@proton.me>
2022-12-06 19:59:06 +0100

    Advanced a bit but not much

--- web-frontend/scripts/lets-make-web-beautiful.js
+++ web-frontend/scripts/lets-make-web-beautiful.js
@@ -6,45 +6,124 @@
 // 3rd stage: adding more complexity and trying to create a multidirectional drawing using the Perlin noise formula
 // 4th stage: It's a duplicata of our 2nd stage but we're using our algorithm used in the last stage to create the drawing
 
-var points = []
-var mult = 0.008
+var density = 10;
+var space = 300 / density;
+var pointsTopLeft;
+var pointsBottomRight;
+var pointsDensity = 0.005;
+var topLeft;
+var bottomRight;
+const offset = 10;
+
+function createVectors(graphics, direction, pointsArray, returned){
+  console.log("Graphics", graphics, "Direction", direction, "PointsArray", pointsArray, "Returned ?", returned);
+  for (var i = 0; i < pointsArray.length; i++) {
+    var r = map(pointsArray[i].x, 0, width, 50, 255);
+    var g = map(pointsArray[i].y, 0, height, 50, 255);
+    var b = map(pointsArray[i].x, 0, width, 255, 50);
+
+    graphics.fill(r,g,b);
+
+    var angle = map(noise(pointsArray[i].x * pointsDensity, pointsArray[i].y * pointsDensity), direction, 0, 1, 720);
+
+    if (returned === true){
+      pointsArray[i].add(createVectors(sin(angle), cos(angle)));
+    } else {
+      pointsArray[i].add(createVector(cos(angle), sin(angle)));
+    };
+
+    graphics.ellipse(pointsArray[i].x, pointsArray[i].y, 1);
+  };
+};
+function setPreferences(graphics, background, angleMode, noiseDetail){
+  graphics.background(background);
+  graphics.angleMode(angleMode);
+  graphics.noiseDetail(noiseDetail);
+};
+function createPoints(pointsArray, width, height, density){
+  console.log(pointsArray)
+  space = width / density;
+  for (var x = 0; x < width; x+= space) {
+    for (var y = 0; y < height; y += space){
+      let p = createVector(x, y);
+      pointsArray.push(p);
+    };
+  };
+};
 
 // We create the drawable where we'll be able to draw later on
 function setup(){
   // At first, it was with the window height and width but then using the Developer tools made the canvas smaller
   // TODO: think of a way to dynamically resize the canvas in order to not lose the focus (and recreate the missing parts
   // TODO: that weren't generated the first time.)
-  createCanvas(400, 400)
-  background(30)
-  angleMode(DEGREES)
-  noiseDetail(10)
+  createCanvas(800, 800);
 
-  var density = 30
-  var space = width / density
+  topLeft = createGraphics(300, 300);
+  bottomRight = createGraphics(300, 300);
 
-  for (var x = 0; x < width; x+= space) {
-    for (var y = 0; y < height; y += space){
-      var p = createVector(x, y)
-      points.push(p)
-    }
-  }
+  setRightBuffer();
 }
 
-function draw(){
-  noStroke()
-  fill(255)
+function draw() {
+    // Draw on your buffers however you like
+    drawLeftBuffer();
+    drawRightBuffer();
+    // Paint the off-screen buffers onto the main canvas
+    image(topLeft, 0, 0);
+    image(bottomRight, 400, 0);
+}
 
-  for (var i = 0; i < points.length; i++) {
-    var r = map(points[i].x, 0, width, 50, 255)
-    var g = map(points[i].y, 0, height, 50, 255)
-    var b = map(points[i].x, 0, width, 255, 50)
+function drawLeftBuffer() {
+  topLeft.background(25);
+  topLeft.angleMode(DEGREES);
+  topLeft.noiseDetail(2);
+  space = 400 / density;
+  const pointsTopLeft = [];
+  for (var x = 0; x < width; x+= space) {
+    for (var y = 0; y < height; y += space){
+      let p = createVector(x, y);
+      pointsTopLeft.push(p);
+    };
+  };
 
-    fill(r,g,b)
 
-    var angle = map(noise(points[i].x * mult, points[i].y * mult), -2, 1, 0, 720)
+  for (var i = 0; i < pointsTopLeft.length; i++) {
+    var r = map(pointsTopLeft[i].x, 0, width, 50, 255);
+    var g = map(pointsTopLeft[i].y, 0, height, 50, 255);
+    var b = map(pointsTopLeft[i].x, 0, width, 255, 50);
 
-    points[i].add(createVector(cos(angle), sin(angle)))
+    topLeft.fill(r,g,b);
 
-    ellipse(points[i].x, points[i].y, 1)
-  }
+    var angle = map(noise(pointsTopLeft[i].x * pointsDensity, pointsTopLeft[i].y * pointsDensity), -2, 0, 1, 720);
+
+    pointsTopLeft[i].add(createVectors(sin(angle), cos(angle)));
+
+    topLeft.ellipse(pointsTopLeft[i].x, pointsTopLeft[i].y, 1);
+  };
+}
+
+function drawRightBuffer() {
+  bottomRight.background(25);
+  bottomRight.angleMode(DEGREES);
+  bottomRight.noiseDetail(2);
+}
+
+function setRightBuffer(){
+  space = 400 / density;
+  for (var x = 0; x < width; x+= space) {
+    for (var y = 0; y < height; y += space){
+      let p = createVector(x, y);
+      pointsBottomRight.push(p);
+    };
+  };
+}
+
+function setLeftBuffer(){
+  space = 400 / density;
+  for (var x = 0; x < width; x+= space) {
+    for (var y = 0; y < height; y += space){
+      let p = createVector(x, y);
+      pointsTopLeft.push(p);
+    };
+  };
 }

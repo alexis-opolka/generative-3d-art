3d18d412b996680f9890388d336454d06a5b758b
alexis-opolka <alexis.opolka@proton.me>
2022-12-13 14:16:12 +0100

    Add repository wide access through web pages

--- web-frontend/scripts/lets-make-web-beautiful.js
+++ web-frontend/scripts/lets-make-web-beautiful.js
@@ -6,50 +6,22 @@
 // 3rd stage: adding more complexity and trying to create a multidirectional drawing using the Perlin noise formula
 // 4th stage: It's a duplicata of our 2nd stage but we're using our algorithm used in the last stage to create the drawing
 
-var density = 10;
+if (innerHeight > innerWidth) {
+  var density = (innerHeight/innerWidth)*10
+} else if (innerWidth > innerHeight) {
+  var density = (innerWidth/innerHeight)*20
+}
+
+//var density = (innerHeight/innerWidth)*20 // (innerWidth/innerHeight)*10;
 var space = 300 / density;
-var pointsTopLeft;
-var pointsBottomRight;
+let pointsTopLeft;
+let pointsBottomRight;
+let unifiedPoints = [];
 var pointsDensity = 0.005;
 var topLeft;
 var bottomRight;
 const offset = 10;
-
-function createVectors(graphics, direction, pointsArray, returned){
-  console.log("Graphics", graphics, "Direction", direction, "PointsArray", pointsArray, "Returned ?", returned);
-  for (var i = 0; i < pointsArray.length; i++) {
-    var r = map(pointsArray[i].x, 0, width, 50, 255);
-    var g = map(pointsArray[i].y, 0, height, 50, 255);
-    var b = map(pointsArray[i].x, 0, width, 255, 50);
-
-    graphics.fill(r,g,b);
-
-    var angle = map(noise(pointsArray[i].x * pointsDensity, pointsArray[i].y * pointsDensity), direction, 0, 1, 720);
-
-    if (returned === true){
-      pointsArray[i].add(createVectors(sin(angle), cos(angle)));
-    } else {
-      pointsArray[i].add(createVector(cos(angle), sin(angle)));
-    };
-
-    graphics.ellipse(pointsArray[i].x, pointsArray[i].y, 1);
-  };
-};
-function setPreferences(graphics, background, angleMode, noiseDetail){
-  graphics.background(background);
-  graphics.angleMode(angleMode);
-  graphics.noiseDetail(noiseDetail);
-};
-function createPoints(pointsArray, width, height, density){
-  console.log(pointsArray)
-  space = width / density;
-  for (var x = 0; x < width; x+= space) {
-    for (var y = 0; y < height; y += space){
-      let p = createVector(x, y);
-      pointsArray.push(p);
-    };
-  };
-};
+const separatedParticles = false;
 
 // We create the drawable where we'll be able to draw later on
 function setup(){
@@ -58,19 +30,62 @@ function setup(){
   // TODO: that weren't generated the first time.)
   createCanvas(800, 800);
 
-  topLeft = createGraphics(300, 300);
-  bottomRight = createGraphics(300, 300);
+  if (separatedParticles) {
+    topLeft = createGraphics(300, 300);
+    bottomRight = createGraphics(300, 300);
 
-  setRightBuffer();
+    setLeftBuffer();
+    setRightBuffer();
+  } else {
+    if (window.innerWidth < 800 && window.innerHeight > 800) {
+      createCanvas(innerWidth, 800)
+    } else if (window.innerHeight < 800 && window.innerWidth > 800) {
+      createCanvas(800, innerHeight)
+    } else if (window.innerHeight < 800 && window.innerWidth < 800) {
+      createCanvas(innerWidth, innerHeight)
+    } else {
+      createCanvas(800, 800);
+    }
+    background(25);
+    angleMode(DEGREES);
+    noiseDetail(2);
+    space = 400 / density;
+
+    for (var x = 0; x < width; x+= space) {
+      for (var y = 0; y < height; y += space){
+        let p = createVector(x, y);
+        unifiedPoints.push(p);
+      }
+    }
+  }
 }
 
 function draw() {
-    // Draw on your buffers however you like
+  if (separatedParticles) {
+        // Draw on your buffers however you like
     drawLeftBuffer();
     drawRightBuffer();
     // Paint the off-screen buffers onto the main canvas
     image(topLeft, 0, 0);
-    image(bottomRight, 400, 0);
+    image(bottomRight, 300, 0);
+  } else {
+    noStroke()
+    fill(255)
+
+    for (var i = 0; i < unifiedPoints.length; i++) {
+      var r = map(unifiedPoints[i].x, 0, width, 50, 255);
+      var g = map(unifiedPoints[i].y, 0, height, 50, 255);
+      var b = map(unifiedPoints[i].x, 0, width, 255, 50);
+
+      fill(r,g,b);
+
+      var angle = map(noise(unifiedPoints[i].x * pointsDensity, unifiedPoints[i].y * pointsDensity), 2, 0, 1, 720);
+
+      unifiedPoints[i].add(createVector(sin(angle), cos(angle)));
+
+      ellipse(unifiedPoints[i].x, unifiedPoints[i].y, 1);
+    }
+  }
 }
 
 function drawLeftBuffer() {
@@ -83,8 +98,8 @@ function drawLeftBuffer() {
     for (var y = 0; y < height; y += space){
       let p = createVector(x, y);
       pointsTopLeft.push(p);
-    };
-  };
+    }
+  }
 
 
   for (var i = 0; i < pointsTopLeft.length; i++) {
@@ -96,34 +111,51 @@ function drawLeftBuffer() {
 
     var angle = map(noise(pointsTopLeft[i].x * pointsDensity, pointsTopLeft[i].y * pointsDensity), -2, 0, 1, 720);
 
-    pointsTopLeft[i].add(createVectors(sin(angle), cos(angle)));
+    pointsTopLeft[i].add(createVector(sin(angle), cos(angle)));
 
     topLeft.ellipse(pointsTopLeft[i].x, pointsTopLeft[i].y, 1);
-  };
+  }
 }
 
 function drawRightBuffer() {
   bottomRight.background(25);
   bottomRight.angleMode(DEGREES);
   bottomRight.noiseDetail(2);
+
+
+  for (var i = 0; i < pointsBottomRight.length; i++) {
+    var r = map(pointsBottomRight[i].x, 0, width, 50, 255);
+    var g = map(pointsBottomRight[i].y, 0, height, 50, 255);
+    var b = map(pointsBottomRight[i].x, 0, width, 255, 50);
+
+    topLeft.fill(r,g,b);
+
+    var angle = map(noise(pointsBottomRight[i].x * pointsDensity, pointsBottomRight[i].y * pointsDensity), -2, 0, 1, 720);
+
+    pointsBottomRight[i].add(createVector(sin(angle), cos(angle)));
+
+    ellipse(pointsBottomRight[i].x, pointsBottomRight[i].y, 1);
+  }
 }
 
 function setRightBuffer(){
+  pointsBottomRight = [];
   space = 400 / density;
   for (var x = 0; x < width; x+= space) {
     for (var y = 0; y < height; y += space){
       let p = createVector(x, y);
       pointsBottomRight.push(p);
-    };
-  };
+    }
+  }
 }
 
 function setLeftBuffer(){
+  pointsTopLeft = [];
   space = 400 / density;
   for (var x = 0; x < width; x+= space) {
     for (var y = 0; y < height; y += space){
       let p = createVector(x, y);
       pointsTopLeft.push(p);
-    };
-  };
+    }
+  }
 }
